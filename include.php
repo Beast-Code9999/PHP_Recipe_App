1d4c9209d67e723c5b915d71cd1cc91d1b898e1f
beastcode <beastcode9999@gmail.com>
2025-04-21 23:41:11 +0930

    debug errors on select form and echo

--- include.php
+++ include.php
@@ -16,12 +16,14 @@
         <select name="page" id="page">
             <option name="">Please select a recipe</option>
             <option value="citrus_salmon" <?php if (!empty($_GET['page']) && $_GET['page'] === 'citrus_salmon') echo 'selected'; ?>>Citrus Salmon</option>
-            <option value="mediterranina" <?php if (!empty($_GET['page']) && $_GET['page'] === 'mediterranina') echo 'selected'; ?>>Mediterranian Pasta</option>
+            <option value="mediterranian" <?php if (!empty($_GET['page']) && $_GET['page'] === 'mediterranian') echo 'selected'; ?>>Mediterranian Pasta</option>
             <option value="sunset_risotto" <?php if (!empty($_GET['page']) && $_GET['page'] === 'sunset_risotto') echo 'selected'; ?>>Sunset Risotto</option>
             <option value="tropical_tacos" <?php if (!empty($_GET['page']) && $_GET['page'] === 'tropical_tacos') echo 'selected'; ?>>Tropical Tango Tacos</option>
         </select>
         <input type="submit" value="submit">
     </form>
-
+    <?php
+        echo file_get_contents("pages/{$_GET['page']}.html");
+    ?>
 </body>
 </html>
