f2a5d4fce5053ed030412a30039bde9aa3a77fa8
beastcode <beastcode9999@gmail.com>
2025-04-25 22:16:42 +0930

    change back to GET after testing

--- include.php
+++ include.php
@@ -12,19 +12,19 @@
         }
     ?>
 
-    <form action="include.php" method="POST">
+    <form action="include.php" method="GET">
         <select name="page" id="page">
             <option name="">Please select a recipe</option>
-            <option value="citrus_salmon" <?php if (!empty($_POST['page']) && $_POST['page'] === 'citrus_salmon') echo 'selected'; ?>>Citrus Salmon</option>
-            <option value="mediterranian" <?php if (!empty($_POST['page']) && $_POST['page'] === 'mediterranian') echo 'selected'; ?>>Mediterranian Pasta</option>
-            <option value="sunset_risotto" <?php if (!empty($_POST['page']) && $_POST['page'] === 'sunset_risotto') echo 'selected'; ?>>Sunset Risotto</option>
-            <option value="tropical_tacos" <?php if (!empty($_POST['page']) && $_POST['page'] === 'tropical_tacos') echo 'selected'; ?>>Tropical Tango Tacos</option>
+            <option value="citrus_salmon" <?php if (!empty($_GET['page']) && $_GET['page'] === 'citrus_salmon') echo 'selected'; ?>>Citrus Salmon</option>
+            <option value="mediterranian" <?php if (!empty($_GET['page']) && $_GET['page'] === 'mediterranian') echo 'selected'; ?>>Mediterranian Pasta</option>
+            <option value="sunset_risotto" <?php if (!empty($_GET['page']) && $_GET['page'] === 'sunset_risotto') echo 'selected'; ?>>Sunset Risotto</option>
+            <option value="tropical_tacos" <?php if (!empty($_GET['page']) && $_GET['page'] === 'tropical_tacos') echo 'selected'; ?>>Tropical Tango Tacos</option>
         </select>
         <input type="submit" value="submit">
     </form>
     <?php
-    if (!empty($_POST['page'])) {
-        echo file_get_contents("pages/{$_POST['page']}.html");
+    if (!empty($_GET['page'])) {
+        echo file_get_contents("pages/{$_GET['page']}.html");
     }
     ?>
 </body>
